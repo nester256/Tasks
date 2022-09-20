1789a2b4f0ac9538d183ec45071d6f12ea13d5e0
nester256 <nestvlad2004@gmail.com>
2022-09-20 16:15:28 +0300

    Add lection code for 20.09.22

---
+++ Practice/20.09.22.py
@@ -0,0 +1,31 @@
+import string
+
+
+def clean_text(text):
+    for sym in string.punctuation:
+        text = text.replace(sym, "")
+    return text.lower().split()
+
+
+def get_second(value):
+    return value[1]
+
+
+def count_words(words):
+    # 1
+    # counted_words = {}
+    # for word in set(words):
+    #     counted_words[word] = words.count(word)
+    #
+    # 2
+    counted_words = {word: words.count(word) for word in set(words)}
+    return sorted(counted_words.items(), key=get_second, reverse=True)
+
+
+def top_10(text):
+    print("***\tTOP 10\t***")
+    for word, num in count_words(clean_text(text))[:10]:
+        print("{0}\t{1}".format(word, num))
+
+
+print(top_10("Привет я Питон, но не просто питон, а питон питонов. Всем питонам питон. Ну всё я попитонил"))
