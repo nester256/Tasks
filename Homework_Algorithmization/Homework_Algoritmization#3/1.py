75d32acea176f6597705eecd5219ea8ac29324dd
nester256 <nestvlad2004@gmail.com>
2022-12-09 23:44:51 +0300

    add to Homework Algoritmization#3 comments for code

--- Homework_Algorithmization/Homework_Algoritmization#3/1.py
+++ Homework_Algorithmization/Homework_Algoritmization#3/1.py
@@ -3,24 +3,24 @@ from typing import List
 
 
 def numEnclaves(grid: List[List[int]]) -> int:
-    count = 0
-    exited = []
+    count = 0 # Количество клеток котрые не выходят за край
+    exited = [] # Сюда буду записывать координаты тех клеток кторые выходят
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                if (i + 1) == len(grid) or i == 0 and (j + 1) == len(grid[0]) or j == 0:
+        for j in range(len(grid[0])): # цикл по всем клеткам
+            if grid[i][j] == 1: # если клетка равна единице
+                if (i + 1) == len(grid) or i == 0 and (j + 1) == len(grid[0]) or j == 0: # условике на поиск райних клеток земли
                     exited.append((i, j))
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1 and exited.count((i, j)) == 0:
+        for j in range(len(grid[0])): # цикл по всем клеткам
+            if grid[i][j] == 1 and exited.count((i, j)) == 0: # Если клетки нет в списке и огна земля
                 if exited.count((i+1, j)) == 1 or\
                         exited.count((i-1, j)) == 1 or\
                         exited.count((i, j+1)) == 1 or\
-                        exited.count((i, j-1)) == 1:
-                    exited.append((i, j))
+                        exited.count((i, j-1)) == 1: # проверяем не содержатся ли её соседи в списке
+                    exited.append((i, j)) # Если содержится добавялем её в список тех выходящих
                 else:
-                    count += 1
-    return count
+                    count += 1 # Увеличиваем счётчик если она не соответствует всем верхним условиям
+    return count # Возвращаем количество клеток которые не выходят за край
 
 
 grid = [[0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 0, 0]]
--- Homework_Algorithmization/Homework_Algoritmization#3/2.py
+++ Homework_Algorithmization/Homework_Algoritmization#3/2.py
@@ -3,9 +3,9 @@ from typing import List
 
 
 def closedIsland(self, grid: List[List[int]]) -> int:
-    res = 0
+    res = 0 # счётчик количества закрытых островов
 
-    def dfs(i, j):
+    def dfs(i, j): # метод с помощью рекурсии идёт по всем соседним клеточкам земли и делает их водой
         if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] != 1:
             grid[i][j] = 1
             dfs(i, j - 1)
@@ -14,7 +14,7 @@ def closedIsland(self, grid: List[List[int]]) -> int:
             dfs(i + 1, j)
 
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[0])): # цикл по всем крайним клеточкам и делает их водой если они являются землёй
             if i == 0 or\
                 j == 0 or\
                     i == len(grid) - 1 or\
@@ -22,9 +22,9 @@ def closedIsland(self, grid: List[List[int]]) -> int:
                 dfs(i, j)
 
     for i in range(len(grid)):
-        for j in range(len(grid[0])):
+        for j in range(len(grid[0])): # цикл по клеточкам земли оставшимся после предыдущих изменений
             if grid[i][j] == 0:
-                dfs(i, j)
-                res += 1
+                dfs(i, j) # преврящает её и её соседей что бы не посчитать ещё раз
+                res += 1 # увеличивает счётчик
 
-    return res
+    return res # возвращает счётчик
