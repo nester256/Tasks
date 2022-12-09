389866c1a934e59fa69884e94e18413b0dbcc8dc
nester256 <nestvlad2004@gmail.com>
2022-12-09 23:55:27 +0300

    add to Homework Algoritmization#3 comments for code

--- Homework_Algorithmization/Homework_Algoritmization#3/3.py
+++ Homework_Algorithmization/Homework_Algoritmization#3/3.py
@@ -6,6 +6,7 @@ class TreeNode:
         self.right = right
 
 
+# Объявляем наше дерево :)
 tree = TreeNode(3)
 tree.left = TreeNode(9)
 tree.right = TreeNode(20)
@@ -14,23 +15,23 @@ tree.left.right = TreeNode(15)
 
 
 def averageTreeBinary(root: TreeNode):
-    queue = []
-    result = []
-    queue.append(root)
+    queue = [] # очередь для уровней
+    result = [] # список результатов
+    queue.append(root) # добавляем голову нашей змеи :)
 
-    while (queue):
-        qlen = len(queue)
-        values = 0
-        for i in range(qlen):
-            node = queue.pop(0)
-            values += node.val
+    while (queue): # Пока наше дерево есть мы идём по его уровням
+        qlen = len(queue) # длинна очереди (т.е сколько на этом уровне есть ячеек)
+        values = 0 # сюда будем добавялть значения всех ячеек уровня
+        for i in range(qlen): # цикл по всем ячейкам на уровне
+            node = queue.pop(0) # Убираем ячейку из очереди и работаем с ней
+            values += node.val # Добавляем значение ячейки
             # print(node.val, qlen)
             if node.left:
                 queue.append(node.left)
             if node.right:
-                queue.append(node.right)
-        result.append(values / qlen)
-    return result
+                queue.append(node.right) # Ищем более низкие уровни и добавляем их в очередь
+        result.append(values / qlen) # добавляем среднее значене нашего уровня
+    return result # Возвращаем результат наших вычислений
 
 
 print(averageTreeBinary(tree))
