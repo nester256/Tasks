0497a7eff533f0047630373cfe7feb18ab749f2d
nester256 <nestvlad2004@gmail.com>
2022-12-10 00:14:32 +0300

    add to Homework Algoritmization#3 comments for code

--- Homework_Algorithmization/Homework_Algoritmization#3/5.py
+++ Homework_Algorithmization/Homework_Algoritmization#3/5.py
@@ -10,36 +10,36 @@ tree = TreeNode(1)
 tree.left = TreeNode(2)
 tree.right = TreeNode(3)
 tree.left.left = TreeNode(5)
+                                        # Объявляем дерево
 
 
 def binaryTreePaths(root):
-    queue = []                          # очередь для уровней
-    result = []                         # сюда будем записывать результат
-    queue.append(root) #
-    left = str(queue[0].val) + "->" #
-    right = str(queue[0].val) + "->" #
+    queue = []                              # очередь для уровней
+    result = []                             # сюда будем записывать результат
+    queue.append(root)                      # добавляем начало нашего дерева :)
+    left = str(queue[0].val) + "->"         # добавляем первое значение в список левых значений
+    right = str(queue[0].val) + "->"        # добавляем первое значение в список правых значений
 
-    while (queue): #
-        qlen = len(queue) #
-        values = 0 #
-        for i in range(qlen): #
-            node = queue.pop(0) #
-            if node.left: #
-                queue.append(node.left) #
-                if node.left.left: #
-                    left += "{}->".format(node.left.val) #
-                else: #
-                    left += "{}".format(node.left.val) #
-            if node.right: #
-                queue.append(node.right) #
-                if node.right.right: #
-                    right += "{0}->".format(node.right.val) #
-                else: #
-                    right += "{0}".format(node.right.val) #
+    while (queue):                          # Пока наше дерево есть мы идём по его уровням
+        qlen = len(queue)                   # длинна очереди (т.е сколько на этом уровне есть ячеек)
+        for i in range(qlen):               # цикл по всем ячейкам на уровне
+            node = queue.pop(0)             # Убираем ячейку из очереди и работаем с ней
+            if node.left:                   # Если есть слева
+                queue.append(node.left)     # Добавляем значение в очередь
+                if node.left.left:          # Если есть слева слева значение
+                    left += "{}->".format(node.left.val)    # То добавляем значение слева его в список с ->
+                else:                                       # Если нет
+                    left += "{}".format(node.left.val)      # То добаялем без штуки
+            if node.right:                                  # Всё то же самое с правой веткой
+                queue.append(node.right)
+                if node.right.right:
+                    right += "{0}->".format(node.right.val)
+                else:
+                    right += "{0}".format(node.right.val)
 
-    result.append(left) #
-    result.append(right) #
-    return result #
+    result.append(left)                                     # Добавляем левую ветку
+    result.append(right)                                    # Добавляем правую ветку
+    return result                                           # Возвращаем все значения
 
 
 print(binaryTreePaths(tree))
